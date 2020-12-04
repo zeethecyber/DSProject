532b4745a8c3f9b92977a97daf0b74c6f4ea58eb
Zeeshan044 <zeeshanasif008@outlook.com>
2020-12-04 12:25:11 +0500

    CPP file created

---
+++ cmake-build-debug/CMakeFiles/DSProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DSProject.dir/main.cpp.o"
+  "DSProject"
+  "DSProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DSProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
