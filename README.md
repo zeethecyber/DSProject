af8b1fd9d888800b36efdc08c8541287169bf9b6
Zeeshan044 <zeeshanasif008@outlook.com>
2020-12-04 15:53:38 +0500

    added linked list, add funtion and display function

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# DSProject
+# Data Structures Project
 This is a private project of Library Management System using C++.
+It should have the functionality of add record, search record, Update record, and delete record using linked list. Following functions should be implemented.
+
+1) Insert a Book
+2) Issue a Book to student
+3) Book Deposit
+4) Find all Issued Books
+5) Stock of Book
+6) Search of Book by ISBN
+7) Display Books by author
+8) Modify a Book record
+9) Delete a Book
--- main.cpp
+++ main.cpp
@@ -1,6 +1,83 @@
+/*This is a Library Management System created with C++
+This program uses data structures implementation
+It will have the following function
+ 1- Add funtion
+ 2- Search Funtion
+ 3- Update Function
+ 4- Delete Function
+ */
+
 #include "iostream"
 using namespace std;
+
+
+// This is a Linked List Class
+class bookList
+{
+private:
+//    Creating Structures for Book bookNode
+    struct bookNode
+    {
+        int serialNo;
+        string bookName;
+        string author;
+        bool issued=false;  //to check if book is issued or not
+        bookNode *nextNode;     //link of next bookNode
+    };
+    int totalBooks;
+    bookNode *headNode=new bookNode();      //headNode node of bookNodes
+    bookNode *tailNode;                     //tail node of bookNodes
+    bookNode *currentNode;                  //this will carry the input from user
+    bookNode *temp;                         //this pointer will be used to iterate between book nodes
+public:
+    bookList()
+    {
+        totalBooks=0;
+    }
+//    inserting bookNode element at the of the list
+    void addBook(int srNo,string name,string auth)
+    {
+        currentNode=new bookNode();     //Creating new book node
+//      Inserting data
+        currentNode->serialNo=srNo;
+        currentNode->bookName=name;
+        currentNode->author=auth;
+        currentNode->nextNode= nullptr;
+//      Checking if there are some already or not
+        if(totalBooks==0)
+        {
+//          Creating the first head and tail node
+            headNode=currentNode;
+            tailNode=currentNode;
+            totalBooks++;
+        }
+//      if there is 1 or more books already in the list. This funtion will add another book at the end of the list.
+        else{
+            tailNode->nextNode=currentNode;
+            tailNode=currentNode;
+            totalBooks++;
+        }
+    }
+//      Function to display all books in the linked list
+    void display()
+    {
+        temp=headNode;
+        for (int i = 0; i < totalBooks; ++i) {
+            cout <<"Serial Number :: "<< temp->serialNo << endl;
+            cout <<"Book Name :: "<< temp->bookName << endl;
+            cout <<"Author Name :: "<< temp->author << endl;
+            cout<<"----------------------------------------------"<<endl;
+            temp=temp->nextNode;
+        }
+    }
+};
+
 int main()
 {
-    cout<<"Hello World";
+    bookList list1;
+    list1.addBook(100,"Computer Science","Zeeshan Asif");
+    list1.addBook(101,"Data Structures & Algorithm","Muneeb Akram");
+    list1.addBook(102,"Programming Fundamentals","Hamza Ismail");
+    list1.addBook(103,"Mathematics","Unknown");
+    list1.display();
 }
